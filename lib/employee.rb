6483acf1960eeadb8e4cec1df0e6e362b3d72f30
vagrant <kiel.strang@gmail.com>
2017-10-10 21:32:39 +0000

    Exercise 8

--- lib/employee.rb
+++ lib/employee.rb
@@ -3,4 +3,11 @@ class Employee < ActiveRecord::Base
   validates :first_name, :last_name, :store, presence: true
   validates :hourly_rate, numericality: { greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
 
+  before_create :set_password
+
+  private
+  PASSWORD_LENGTH = 8
+  def set_password
+    self.password = rand(36**PASSWORD_LENGTH).to_s(36)
+  end
 end
