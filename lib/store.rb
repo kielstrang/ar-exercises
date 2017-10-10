2e8c77edd332dee672ebdf99faf7833e28d29ddf
vagrant <kiel.strang@gmail.com>
2017-10-10 21:36:09 +0000

    Exercise 9

--- lib/store.rb
+++ lib/store.rb
@@ -3,9 +3,16 @@ class Store < ActiveRecord::Base
   validates :name, length: { minimum: 3 }
   validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }
   validate :has_clothes
+  before_destroy :check_for_employees
 
+  private
   def has_clothes
     mens_apparel || womens_apparel
   end
 
+  def check_for_employees
+    if employees.count > 0
+      throw :abort
+    end
+  end
 end
